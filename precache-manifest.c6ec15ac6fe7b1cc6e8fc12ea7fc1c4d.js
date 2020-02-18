9327077694c1478754bf476aab3dd11c195a0ba3
Ekpenyong <okpo.ekpenyong@gmail.com>
2020-02-18 06:21:57 +0100

    Updates

---
+++ precache-manifest.c6ec15ac6fe7b1cc6e8fc12ea7fc1c4d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2773979b7067baf644ef27848fd5b417",
+    "url": "/vesoft/index.html"
+  },
+  {
+    "revision": "d800a299211d27a0b224",
+    "url": "/vesoft/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9e30f92264bb6286db74",
+    "url": "/vesoft/static/css/main.7b6520b8.chunk.css"
+  },
+  {
+    "revision": "d800a299211d27a0b224",
+    "url": "/vesoft/static/js/2.bea42959.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/vesoft/static/js/2.bea42959.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e30f92264bb6286db74",
+    "url": "/vesoft/static/js/main.9d64ad54.chunk.js"
+  },
+  {
+    "revision": "5005d918f52ff06bb4cf",
+    "url": "/vesoft/static/js/runtime-main.ac545dd7.js"
+  },
+  {
+    "revision": "59f7e4b64ae6e75563e5f31786289a01",
+    "url": "/vesoft/static/media/bg-pics.59f7e4b6.png"
+  },
+  {
+    "revision": "6c9295dcbbb4e526668f656ebdeb3b38",
+    "url": "/vesoft/static/media/internet.6c9295dc.svg"
+  },
+  {
+    "revision": "ee7ea99ec121c98352aa293172d0aa9f",
+    "url": "/vesoft/static/media/study.ee7ea99e.jpg"
+  },
+  {
+    "revision": "13f04b45ec311f82505be0848471ff2d",
+    "url": "/vesoft/static/media/vesoft_wwd.13f04b45.png"
+  }
+]);
